2ef6db3107af8378ae78d919acf8b45f8352754e
tomocy <tomocy.dev@gmail.com>
2020-01-16 18:02:24 +0900

    use as ordered src

--- main.go
+++ main.go
@@ -1,102 +1,59 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"strings"
-	"sync"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	ps := newpubsub()
-	defer ps.close()
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	go ps.serve(ctx)
-
-	subs := make([]chan string, 3)
-	for i := range subs {
-		subs[i] = make(chan string)
-	}
-
-	for _, sub := range subs {
-		ps.subscribe(sub)
-	}
-
-	var wg sync.WaitGroup
-	for i, sub := range subs {
-		wg.Add(1)
-		go func(msgCh <-chan string, cnt int) {
-			defer wg.Done()
-			for msg := range msgCh {
-				fmt.Println(strings.Repeat(msg, cnt))
-			}
-		}(sub, i+1)
-	}
-
-	for i := 0; i < 10; i++ {
-		ps.publish(fmt.Sprint(i))
-	}
-
-	for _, sub := range subs {
-		ps.unsubscribe(sub)
-		close(sub)
-	}
-
-	wg.Wait()
-}
-
-func newpubsub() *pubsub {
-	return &pubsub{
-		subs:    make(map[sub]struct{}),
-		subCh:   make(chan sub),
-		unsubCh: make(chan sub),
-		msgCh:   make(chan string),
+	for line := range transform(fetch()) {
+		fmt.Println(line)
 	}
 }
 
-type pubsub struct {
-	subs    map[sub]struct{}
-	subCh   chan sub
-	unsubCh chan sub
-	msgCh   chan string
-}
+func transform(srcCh <-chan string) <-chan string {
+	ch := make(chan string)
 
-func (ps *pubsub) subscribe(sub sub) {
-	ps.subCh <- sub
-}
-
-func (ps *pubsub) unsubscribe(sub sub) {
-	ps.unsubCh <- sub
-}
-
-func (ps *pubsub) publish(msg string) {
-	ps.msgCh <- msg
-}
-
-func (ps *pubsub) serve(ctx context.Context) {
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case ch := <-ps.subCh:
-			ps.subs[ch] = struct{}{}
-		case ch := <-ps.unsubCh:
-			delete(ps.subs, ch)
-		case msg := <-ps.msgCh:
-			for sub := range ps.subs {
-				sub <- msg
-			}
+	go func() {
+		defer close(ch)
+		for src := range srcCh {
+			time.Sleep(time.Duration(rand.Intn(5)) * time.Millisecond)
+			ch <- fmt.Sprintf("%s was fetched.", src)
 		}
-	}
+	}()
+
+	return ch
 }
 
-func (ps *pubsub) close() {
-	close(ps.subCh)
-	close(ps.unsubCh)
-	close(ps.msgCh)
-}
+func fetch() <-chan string {
+	chCh := make(chan chan string, 10)
 
-type sub chan<- string
+	go func() {
+		defer close(chCh)
+
+		for i := 0; i < 1000; i++ {
+			ch := make(chan string)
+			chCh <- ch
+
+			go func(i int) {
+				defer close(ch)
+
+				time.Sleep(time.Duration(rand.Intn(5)) * time.Millisecond)
+				ch <- fmt.Sprintf("line:%d", i)
+			}(i)
+		}
+	}()
+
+	ch := make(chan string)
+
+	go func() {
+		defer close(ch)
+
+		for srcCh := range chCh {
+			ch <- <-srcCh
+		}
+	}()
+
+	return ch
+}
