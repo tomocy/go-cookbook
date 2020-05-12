24c1d9981024aa7e07a13425f08f1c157fd7dbfe
tomocy <tomocy.dev@gmail.com>
2020-05-12 19:11:48 +0900

    oauth/resource: update HTTPServer
    HTTPServer can create users.

--- oauth/resource/gateway/controller/http.go
+++ oauth/resource/gateway/controller/http.go
@@ -7,15 +7,18 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/tomocy/go-cookbook/oauth/resource"
+	"github.com/tomocy/go-cookbook/oauth/resource/usecase"
 )
 
 func NewHTTPServer(
 	w io.Writer, addr string,
+	ren httpServerRenderer,
 	userServ resource.UserService, userRepo resource.UserRepo,
 ) HTTPServer {
 	return HTTPServer{
 		w:        w,
 		addr:     addr,
+		renderer: ren,
 		userServ: userServ,
 		userRepo: userRepo,
 	}
@@ -24,6 +27,7 @@ func NewHTTPServer(
 type HTTPServer struct {
 	w        io.Writer
 	addr     string
+	renderer httpServerRenderer
 	userServ resource.UserService
 	userRepo resource.UserRepo
 }
@@ -31,8 +35,65 @@ type HTTPServer struct {
 func (s HTTPServer) Run() error {
 	r := chi.NewRouter()
 
+	r.Route("/users", func(r chi.Router) {
+		r.Post("/", handlerFunc(s.createUser()))
+	})
+
 	s.logfln("listen and serve on %s", s.addr)
 	return http.ListenAndServe(s.addr, r)
+}
+
+func handlerFunc(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		h.ServeHTTP(w, r)
+	}
+}
+
+func (s HTTPServer) createUser() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if err := r.ParseForm(); err != nil {
+			s.renderErr(s.renderer.RenderCreateUserErr, w, "parse form", err)
+			return
+		}
+
+		var (
+			name  = r.PostFormValue("name")
+			email = r.PostFormValue("email")
+			pass  = r.PostFormValue("password")
+		)
+		create := usecase.NewCreateUser(s.userServ, s.userRepo)
+		user, err := create.Do(name, email, pass)
+		if err != nil {
+			s.renderErr(s.renderer.RenderCreateUserErr, w, "create user", err)
+			return
+		}
+
+		if err := s.renderer.RenderCreatedUser(w, user); err != nil {
+			s.renderErr(s.renderer.RenderCreateUserErr, w, "render user", err)
+		}
+	})
+}
+
+func (s HTTPServer) renderErr(render errRenderer, w http.ResponseWriter, did string, err error) {
+	if resource.IsErrInput(err) {
+		s.renderErrMessage(render, w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	s.logfln("failed to %s: %s", did, err)
+	s.renderErrStatus(render, w, http.StatusInternalServerError)
+}
+
+func (s HTTPServer) renderErrStatus(render errRenderer, w http.ResponseWriter, code int) {
+	s.renderErrMessage(render, w, code, http.StatusText(code))
+}
+
+func (s HTTPServer) renderErrMessage(render errRenderer, w http.ResponseWriter, code int, msg string) {
+	w.WriteHeader(code)
+	if err := render(w, fmt.Errorf(msg)); err != nil {
+		s.logfln("failed to render error with message: %s", err)
+		return
+	}
 }
 
 func (s HTTPServer) logfln(format string, as ...interface{}) {
@@ -42,3 +103,10 @@ func (s HTTPServer) logfln(format string, as ...interface{}) {
 func (s HTTPServer) logf(format string, as ...interface{}) {
 	fmt.Fprintf(s.w, format, as...)
 }
+
+type httpServerRenderer interface {
+	RenderCreatedUser(io.Writer, resource.User) error
+	RenderCreateUserErr(io.Writer, error) error
+}
+
+type errRenderer func(io.Writer, error) error
