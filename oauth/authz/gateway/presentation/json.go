18cc0712b088e1bc0d4cd93907ff00dc0619546b
tomocy <tomocy.dev@gmail.com>
2020-05-13 19:18:47 +0900

    oauth/authz: update json
    JSON can render access tokens.

--- oauth/authz/gateway/presentation/json.go
+++ oauth/authz/gateway/presentation/json.go
@@ -4,15 +4,19 @@ import (
 	jsonPkg "encoding/json"
 	"io"
 
-	"github.com/tomocy/go-cookbook/oauth"
+	"github.com/tomocy/go-cookbook/oauth/dto"
 )
 
 var JSON json
 
 type json struct{}
 
-func (json) RenderIntrospection(w io.Writer, intro oauth.Introspection) error {
+func (json) RenderIntrospection(w io.Writer, intro dto.Introspection) error {
 	return jsonPkg.NewEncoder(w).Encode(intro)
+}
+
+func (json) RenderAccessToken(w io.Writer, tok dto.AccessToken) error {
+	return jsonPkg.NewEncoder(w).Encode(tok)
 }
 
 func (json) RenderErr(w io.Writer, err error) error {
