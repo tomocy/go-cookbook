f5e43f1023914dea714a2b4e74bd135a57d3b3ac
tomocy <tomocy.dev@gmail.com>
2020-05-14 11:20:02 +0900

    oauth/app: fix UserRepo

--- oauth/app/infra/memory/user.go
+++ oauth/app/infra/memory/user.go
@@ -1,4 +1,4 @@
-package mock
+package memory
 
 import (
 	"context"
@@ -6,20 +6,22 @@ import (
 	"github.com/tomocy/go-cookbook/oauth/app"
 )
 
-var UserRepo = userRepo{
-	users: make(map[app.UserID]app.User),
+func NewUserRepo() *UserRepo {
+	return &UserRepo{
+		users: make(map[app.UserID]app.User),
+	}
 }
 
-type userRepo struct {
+type UserRepo struct {
 	users map[app.UserID]app.User
 }
 
-func (r userRepo) Find(_ context.Context, id app.UserID) (app.User, bool, error) {
+func (r UserRepo) Find(_ context.Context, id app.UserID) (app.User, bool, error) {
 	u, ok := r.users[id]
 	return u, ok, nil
 }
 
-func (r *userRepo) Save(_ context.Context, user app.User) error {
+func (r *UserRepo) Save(_ context.Context, user app.User) error {
 	r.users[user.ID()] = user
 	return nil
 }
