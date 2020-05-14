91bc32dd269085575ac25eb02e34370e379d82ee
tomocy <tomocy.dev@gmail.com>
2020-05-14 11:41:14 +0900

    oauth/app: add AddProvider

--- oauth/app/usecase/usecase.go
+++ oauth/app/usecase/usecase.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/tomocy/go-cookbook/oauth/app"
 )
@@ -25,4 +26,40 @@ func (u FindUser) Do(id app.UserID) (app.User, bool, error) {
 	}
 
 	return user, found, nil
+}
+
+func NewAddProvider(repo app.UserRepo) AddProvider {
+	return AddProvider{
+		repo: repo,
+	}
+}
+
+type AddProvider struct {
+	repo app.UserRepo
+}
+
+func (u AddProvider) Do(id app.UserID, name, tok string) error {
+	ctx := context.TODO()
+
+	user, found, err := u.repo.Find(ctx, id)
+	if err != nil {
+		return fmt.Errorf("failed to find user: %w", err)
+	}
+	if !found {
+		return app.ErrInvalidArg("no such user")
+	}
+
+	prov, err := app.NewProvider(name, tok)
+	if err != nil {
+		return fmt.Errorf("failed to create provider: %w", err)
+	}
+	if err := user.AddProvider(prov); err != nil {
+		return err
+	}
+
+	if err := u.repo.Save(ctx, user); err != nil {
+		return fmt.Errorf("failed to save user: %w", err)
+	}
+
+	return nil
 }
