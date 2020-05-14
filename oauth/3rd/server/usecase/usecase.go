ce37e67bf0a8fcb68eaa9dcbc2ebbbc6e4a6f6cb
tomocy <tomocy.dev@gmail.com>
2020-05-14 17:06:09 +0900

    oauth/3rd/server: fix DoHaeAccessToken

--- oauth/3rd/server/usecase/usecase.go
+++ oauth/3rd/server/usecase/usecase.go
@@ -10,7 +10,7 @@ import (
 type DoHaveAccessToken struct{}
 
 func (u DoHaveAccessToken) Do(id string) (bool, error) {
-	return id != "test_user_id", nil
+	return id == "test_user_id", nil
 }
 
 type GenerateAuthzCodeURI struct{}
