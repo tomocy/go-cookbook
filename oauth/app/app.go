848fdca534facabfa85d766812550541f387f8cd
tomocy <tomocy.dev@gmail.com>
2020-05-14 10:17:10 +0900

    oauth/app: add NewUser

--- oauth/app/app.go
+++ oauth/app/app.go
@@ -1,13 +1,48 @@
 package app
 
+func NewUser(id UserID) (User, error) {
+	var u User
+	if err := u.setID(id); err != nil {
+		return User{}, err
+	}
+
+	return u, nil
+}
+
 type User struct {
 	id UserID
+}
+
+func (u User) ID() UserID {
+	return u.id
+}
+
+func (u *User) setID(id UserID) error {
+	if id == "" {
+		return ErrInvalidArg("id should not be empty")
+	}
+
+	u.id = id
+
+	return nil
 }
 
 type UserID string
 
 type Provider struct {
-	name   string
-	userID UserID
-	tok    string
+	name string
+	tok  string
 }
+
+type errInput interface {
+	error
+	ErrInput()
+}
+
+type ErrInvalidArg string
+
+func (e ErrInvalidArg) Error() string {
+	return string(e)
+}
+
+func (ErrInvalidArg) ErrInput() {}
