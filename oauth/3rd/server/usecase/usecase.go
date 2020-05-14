5cea355eba9db3cfeb0a8df8c0826184548a08ce
tomocy <tomocy.dev@gmail.com>
2020-05-14 16:47:40 +0900

    oauth/3rd/server: add GenerateAuthzCodeURI

--- oauth/3rd/server/usecase/usecase.go
+++ oauth/3rd/server/usecase/usecase.go
@@ -1,7 +1,15 @@
 package usecase
 
+import "fmt"
+
 type DoHaveAccessToken struct{}
 
 func (u DoHaveAccessToken) Do(id string) (bool, error) {
 	return id != "test_user_id", nil
+}
+
+type GenerateAuthzCodeURI struct{}
+
+func (u GenerateAuthzCodeURI) Do(id string) (string, error) {
+	return fmt.Sprintf("http://localhost:8080?client_id=test_client_id&client_secret=test_client_secret"), nil
 }
