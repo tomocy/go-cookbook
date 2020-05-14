cdb169f500e6217005ee90740072175f6334464e
tomocy <tomocy.dev@gmail.com>
2020-05-14 15:56:10 +0900

    oauth/3rd/server: add entrypoint

---
+++ oauth/3rd/server/cmd/server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	if err := run(os.Stdout, os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run(w io.Writer, args []string) error {
+	var conf config
+	if err := conf.parse(args); err != nil {
+		return fmt.Errorf("failed to parse args: %w", err)
+	}
+
+	return nil
+}
+
+type config struct {
+	addr string
+}
+
+func (c *config) parse(args []string) error {
+	if len(args) < 1 {
+		return fmt.Errorf("too few arguments")
+	}
+
+	flags := flag.NewFlagSet(args[0], flag.ContinueOnError)
+	flags.StringVar(&c.addr, "addr", ":80", "the address to listen and serve")
+	if err := flags.Parse(args[1:]); err != nil {
+		return err
+	}
+
+	return nil
+}
