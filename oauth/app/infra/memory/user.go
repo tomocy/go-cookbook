2fb605058548da6d6328ccf48eab8763f670ad10
tomocy <tomocy.dev@gmail.com>
2020-05-14 10:57:10 +0900

    oauth/user: fix UserRepo

--- oauth/app/infra/memory/user.go
+++ oauth/app/infra/memory/user.go
@@ -14,6 +14,11 @@ type userRepo struct {
 	users map[app.UserID]app.User
 }
 
+func (r userRepo) Find(_ context.Context, id app.UserID) (app.User, bool, error) {
+	u, ok := r.users[id]
+	return u, ok, nil
+}
+
 func (r *userRepo) Save(_ context.Context, user app.User) error {
 	r.users[user.ID()] = user
 	return nil
