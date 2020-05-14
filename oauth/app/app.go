0ddb6da50e87b607d63ed41a53e1c670ec2566e0
tomocy <tomocy.dev@gmail.com>
2020-05-14 10:23:26 +0900

    oauth/app: update User
    User has providers.

--- oauth/app/app.go
+++ oauth/app/app.go
@@ -1,7 +1,11 @@
 package app
 
+import "fmt"
+
 func NewUser(id UserID) (User, error) {
-	var u User
+	u := User{
+		providers: make(map[string]Provider),
+	}
 	if err := u.setID(id); err != nil {
 		return User{}, err
 	}
@@ -10,7 +14,8 @@ func NewUser(id UserID) (User, error) {
 }
 
 type User struct {
-	id UserID
+	id        UserID
+	providers map[string]Provider
 }
 
 func (u User) ID() UserID {
@@ -27,11 +32,53 @@ func (u *User) setID(id UserID) error {
 	return nil
 }
 
+func (u *User) AddProvider(p Provider) error {
+	if _, ok := u.providers[p.name]; ok {
+		return ErrInvalidArg("duplicated provider")
+	}
+
+	u.providers[p.name] = p
+
+	return nil
+}
+
 type UserID string
+
+func NewProvider(name, tok string) (Provider, error) {
+	var p Provider
+	if err := p.setName(name); err != nil {
+		return Provider{}, err
+	}
+	if err := p.setToken(tok); err != nil {
+		return Provider{}, err
+	}
+
+	return p, nil
+}
 
 type Provider struct {
 	name string
 	tok  string
+}
+
+func (p *Provider) setName(name string) error {
+	if name == "" {
+		return ErrInvalidArg("name should not be empty")
+	}
+
+	p.name = name
+
+	return nil
+}
+
+func (p *Provider) setToken(tok string) error {
+	if tok == "" {
+		return fmt.Errorf("token should not be empty")
+	}
+
+	p.tok = tok
+
+	return nil
 }
 
 type errInput interface {
